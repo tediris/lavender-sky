c27b07374be53dbf7837f16e16da980c754a7e9e
Thaminda Edirisooriya <tediris@stanford.edu>
2018-06-26 10:58:38 -0700

    testing some stuff with loading a csv

--- deck.rb
+++ deck.rb
@@ -1,6 +1,14 @@
 require 'squib'
 
-Squib::Deck.new(cards: 3, layout: 'layout.yml') do
-  text str: 'Hello, World!'
-  save format: :png
+data = Squib.csv file: 'data.csv'
+
+Squib::Deck.new cards: data['name'].size, layout: 'economy_fix.yml' do
+  background color: 'white'
+  rect layout: 'cut'
+  rect layout: 'safe'
+  text str: data['name'], layout: 'title'
+  text str: data['power'], layout: 'description'
+  text str: Time.now, layout: 'credits'
+  save_png prefix: 'card_'
+  save_pdf trim: 37.5
 end
